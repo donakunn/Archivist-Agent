619e1dcc7a4b5b9fc5905b99ed27be8ec4fe0bbb
donakunn <donalara.l@gmail.com>
2021-09-30 17:23:27 +0200

    aggiunto Porter stemmer + bugfix

--- Classifiers/naive_bayes.py
+++ Classifiers/naive_bayes.py
@@ -1,6 +1,7 @@
 import os
 import re
 import numpy as np
+from nltk.stem.porter import PorterStemmer
 from Classifiers.stopwords import STOPWORDS
 
 PATHSEP = os.path.sep
@@ -27,15 +28,10 @@ class NaiveBayesClassifier:
 
     def nb_init(self):
 
-        if not os.path.exists('./vocabolario.npy'):
+        if not os.path.exists('./vocabolario.npy') or not os.path.exists('./prob_classi.npy')\
+                or not os.path.exists('./prob_parole.npy'):
             train_corpus = self.costruisci_corpus("20news-bydate-train", True)
             print(f"Corpus con {len(train_corpus)} classi e {len(self.vocabolario)} parole diverse")
-
-        # Salva il vocabolario
-        # with open(NOME_VOC, "w") as file:
-        #    for p in vocabolario:
-        #        file.write(p + "\n")
-
             self.training(train_corpus)
         else:
             self.load_classifier_attributes()
@@ -44,8 +40,12 @@ class NaiveBayesClassifier:
         print("\nFase di test: classifica i documenti che non sono nel training set.")
         risultato = self.testing(test_corpus)
 
-        print(f"\nRisultato totale della classificazione: {risultato}% di risposte esatte")
+        print("\nRisultato totale della classificazione: {0:6.2f}% di risposte esatte".format(risultato))
         input("Premere un tasto per terminare...")
+
+    def porter_tokenizer(self, lista_parole):
+        porter = PorterStemmer()
+        return [porter.stem(parola) for parola in lista_parole]
 
     def analizza_testo(self, doc):
         """Tokenizza il testo contenuto in un documento e restituisce una lista che contiene i token estratti
@@ -54,7 +54,8 @@ class NaiveBayesClassifier:
         for parola in re.findall(r"[a-zA-Z]+", doc):
             if parola not in STOPWORDS:
                 lista_parole.append(parola)
-        return lista_parole
+
+        return self.porter_tokenizer(lista_parole)
 
     def costruisci_corpus(self, dir_path, is_training_set):  # nome non mi convince
         """Data una dir_path che contenga solo cartelle,
@@ -65,13 +66,12 @@ class NaiveBayesClassifier:
             dizionario le cui chiavi sono le sottocartelle (le classi) e i cui valori
             sono dizionari le cui chiavi sono i nomi dei file contenuti nelle
             sottocartelle e le liste di parole che sono state estratte da essi."""
-        print("\nCarica nel vocabolario tutte le parole di tutti i documenti")
+        print("\nCorpus in costruzione..")
         corpus = {}
         for root, dirs, files in os.walk(dir_path):
             for d in dirs:
                 corpus[d] = {}
                 # Ora apre tutti i file nella classe
-                print(d)
                 for sroot, sdirs, sfiles in os.walk(dir_path + PATHSEP + d):
                     for f in sfiles:
                         with open(dir_path + PATHSEP + d + PATHSEP + f, "r", encoding="latin-1") as file:
@@ -84,8 +84,8 @@ class NaiveBayesClassifier:
                                     else:
                                         self.vocabolario[t] = 1
 
-                if is_training_set:
-                    self.pulizia_vocabolario()
+        if is_training_set:
+            self.pulizia_vocabolario()
         return corpus
 
     def pulizia_vocabolario(self):
@@ -93,7 +93,7 @@ class NaiveBayesClassifier:
          per migliorare le prestazioni del sistema."""
         da_eliminare = []
         for p in self.vocabolario:
-            if self.vocabolario[p] <= 3:
+            if self.vocabolario[p] >= 15000:
                 da_eliminare.append(p)
         for p in da_eliminare:
             del self.vocabolario[p]
@@ -124,21 +124,22 @@ class NaiveBayesClassifier:
             print("contiene", len(lista_parole), "parole totali (anche ripetute)")
             denominatore = len(lista_parole) + lunghezza_vocabolario
             for p in self.vocabolario:
-                self.prob_parole[p, c] = ((lista_parole.count(p) + 1) / denominatore) * lunghezza_vocabolario
+                self.prob_parole[p, c] = np.log(((lista_parole.count(p) + 1) / denominatore))
 
-            # Calcolo delle probabilità P(c) relative alle classi
-            self.prob_classi[c] = len(corpus[c].keys()) / n_documenti_totali
+        # Calcolo delle probabilità P(c) relative alle classi
+        for c in corpus:
+            self.prob_classi[c] = np.log(len(corpus[c].keys()) / n_documenti_totali)
         self.save_classifier_attributes()
 
     def classifica(self, testo_doc):
         # Cerca la classe che massimizza P(classe)P(token|classe)
-        p_max = -1
+        p_max = -10000000000
         class_max = ""
         for cc in self.prob_classi:
             p = self.prob_classi[cc]
             for t in testo_doc:
                 if t in self.vocabolario:
-                    p *= self.prob_parole[t, cc]
+                    p += self.prob_parole[t, cc]
             if p > p_max:
                 p_max = p
                 class_max = cc
@@ -150,22 +151,24 @@ class NaiveBayesClassifier:
             training_set contiene come chiavi le classi e come valori le liste di
             file che NON vanno utilizzati per il test ???. Restituisce
             la percentuale di risposte esatte."""
-        risposte_esatte = 0
-        risposte_sbagliate = 0
+        tot_esatte = 0
+        tot_errate = 0
         for c in corpus:
+            risposte_esatte = 0
+            risposte_sbagliate = 0
             print("Classe", c, end=" ")
-            e = risposte_esatte
-            s = risposte_sbagliate
-            for testo in corpus[c]:
+            for documento in corpus[c]:
                 # Ora prova a classificare il documento
                 # print("Documento", nome, end="")
-                class_max = self.classifica(testo)
+                class_max = self.classifica(corpus[c][documento])
                 # La classe scelta è class_max
                 # print(class_max, "[", p_max*100, "%]?", c)
                 if class_max == c:
                     risposte_esatte += 1
                 else:
                     risposte_sbagliate += 1
-            print(str(100 * (risposte_esatte - e) / (
-                        risposte_esatte - e + risposte_sbagliate - s)) + "% di risposte esatte")
-        return risposte_esatte / (risposte_esatte + risposte_sbagliate) * 100
+            tot_esatte += risposte_esatte
+            tot_errate += risposte_sbagliate
+            print("{0:6.2f}% di risposte esatte".format((100 * risposte_esatte / (
+                        risposte_esatte + risposte_sbagliate))))
+        return tot_esatte / (tot_esatte + tot_errate) * 100
